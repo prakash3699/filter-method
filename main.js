3282414c515ab1ffb55e7b2a9de3911ec0bde388
prakash3699 <jerryprakash@gmail.com>
2023-03-24 17:04:31 +0530

    filter-method

---
+++ main.js
@@ -0,0 +1,11 @@
+const employees = [
+  { name: 'David Carlson', age: 30 },
+  { name: 'John Cena', age: 34 },
+  { name: 'Mike Sheridan', age: 25 },
+  { name: 'John Carte', age: 50 }
+];
+
+let res=employees.filter(function(filt){
+	return filt.age>=31
+});
+console.log(res)
